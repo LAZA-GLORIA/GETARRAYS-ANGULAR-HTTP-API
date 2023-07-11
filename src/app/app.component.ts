42c60bfbe1c2156e4e06e635da08414a2a1b24e6
LAZA-GLORIA <gloria.besamba@axyus.com>
2023-07-11 14:20:40 +0200

    Implementation et envoie requete GET

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,115 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from './service/user.service';
+import { User } from './interface/user';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'GetArraysAngularHttp';
+
+  private user: any = {
+      "id": 2,
+      "name": "PATCH DOUBLON ID 1 Graham",
+      "username": "DOUBLONID 1",
+      "email": "Sincere@april.biz",
+      // "address": {
+      // "street": "Kulas Light",
+      // "suite": "Apt. 556",
+      // "city": "Gwenborough",
+      // "zipcode": "92998-3874",
+      // "geo": {
+      // "lat": "-37.3159",
+      // "lng": "81.1496"
+      // }
+      // },
+      // "phone": "1-770-736-8031 x56442",
+      // "website": "hildegard.org",
+      // "company": {
+      // "name": "Romaguera-CronaKLKKKKKKKK",
+      // "catchPhrase": "Multi-layered client-server neural-net",
+      // "bs": "harness real-time e-markets"
+      // }
+  }
+  
+
+  constructor(private userService: UserService) {}
+
+  // Appelé à chaque fois que le composant est initialisé
+  ngOnInit(): void {
+    // this.onGetUser();
+    // this.onCreateUser();
+    // this.onUpdateUser();
+    // this.onPatchUser();
+    this.onDeleteUser();
+    this.onGetUsers();
+  }
+
+  onGetUsers(): void {
+    // subscribe est deprecié avec des callback arguments 
+    // il est recommandé d'utiliser un objet observer unique pour gérer les notifications d'une Observable.(observer arguments)
+    this.userService.getUsers().subscribe({
+      next: (response: User[]) => console.table(response),
+      error: (error: any) => console.log(error),
+      complete: () => console.log("Liste de tous les users"),
+    }    
+    );
+  }
+
+  onGetUser(): void {
+    // subscribe est deprecié avec des callback arguments 
+    // il est recommandé d'utiliser un objet observer unique pour gérer les notifications d'une Observable.(observer arguments)
+    this.userService.getUser().subscribe({
+      next: (response: User) => console.log(response),
+      error: (error: any) => console.log(error),
+      complete: () => console.log("Affichage d'un user"),
+    }    
+    );
+  }
+
+  onCreateUser(): void {
+    // subscribe est deprecié avec des callback arguments 
+    // il est recommandé d'utiliser un objet observer unique pour gérer les notifications d'une Observable.(observer arguments)
+    this.userService.createUser(this.user).subscribe({
+      next: (response: User) => console.log(response),
+      error: (error: any) => console.log(error),
+      complete: () => console.log("Le user a été crée! "),
+    }    
+    );
+  }
+
+  onUpdateUser(): void {
+    // subscribe est deprecié avec des callback arguments 
+    // il est recommandé d'utiliser un objet observer unique pour gérer les notifications d'une Observable.(observer arguments)
+    this.userService.updateUser(this.user).subscribe({
+      next: (response: User) => console.log(response),
+      error: (error: any) => console.log(error),
+      complete: () => console.log("Le user a été modifié! "),
+    }    
+    );
+  }
+
+  onPatchUser(): void {
+    // subscribe est deprecié avec des callback arguments 
+    // il est recommandé d'utiliser un objet observer unique pour gérer les notifications d'une Observable.(observer arguments)
+    this.userService.patchUser(this.user).subscribe({
+      next: (response: User) => console.log(response),
+      error: (error: any) => console.log(error),
+      complete: () => console.log("La requet patch a reussi, le user a été modifié! "),
+    }    
+    );
+  }
+
+  onDeleteUser(): void {
+    // subscribe est deprecié avec des callback arguments 
+    // il est recommandé d'utiliser un objet observer unique pour gérer les notifications d'une Observable.(observer arguments)
+    this.userService.deleteUser(5).subscribe({
+      next: (response: unknown) => console.log(response),
+      error: (error: any) => console.log(error),
+      complete: () => console.log("Suppresion du user! "),
+    }    
+    );
+  }
 }
--- src/app/service/user.service.ts
+++ src/app/service/user.service.ts
@@ -2,19 +2,37 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http'
 import { Observable } from 'rxjs';
 import { User } from '../interface/user';
+import { environment } from '../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
+  private apiUrl = environment.apiUrl;
 
   constructor(private http: HttpClient) { }
 
   getUsers(): Observable<User[]> {
-    return this.http.get<User[]>(`https://jsonplaceholder.typicode.com/users`);
+    return this.http.get<User[]>(`${this.apiUrl}/users`);
   }
 
-  getUser(): Observable<User[]> {
-    return this.http.get<User[]>(`https://jsonplaceholder.typicode.com/users/1`);
+  getUser(): Observable<User> {
+    return this.http.get<User>(`${this.apiUrl}/users/1`);
+  }
+
+  createUser(user: User): Observable<User> {
+    return this.http.post<User>(`${this.apiUrl}/users`, user);
+  }
+
+  updateUser(user: User): Observable<User> {
+    return this.http.put<User>(`${this.apiUrl}/users/${user.id}`, user);
+  }
+
+  patchUser(user: User): Observable<User> {
+    return this.http.patch<User>(`${this.apiUrl}/users/${user.id}`, user);
+  }
+
+  deleteUser(id: number): Observable<unknown> {
+    return this.http.delete<unknown>(`${this.apiUrl}/users/${id}`);
   }
 }
